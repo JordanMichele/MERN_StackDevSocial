1456ebaf29eafb4d0056c59a6ccda499dbe55e78
Jordan Michele <jordanmichele32@gmail.com>
2019-04-13 13:18:11 -0400

    added json web token + bearer"

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://jordan:j252lfqo@ds139277.mlab.com:39277/mernstackdevsocial"
+    "mongodb://jordan:j252lfqo@ds139277.mlab.com:39277/mernstackdevsocial",
+  secretOrKey: "secret"
 };
