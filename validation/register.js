b2590a3e6f716663479215b39be9ed8581ea0038
Jordan Michele <jordanmichele32@gmail.com>
2019-04-17 21:31:53 -0400

    adding Validator

--- validation/register.js
+++ validation/register.js
@@ -3,11 +3,11 @@ const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
-
+  // Using Validator to check name input
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }
-
+  // Using Validator to check if the name field has been inputted
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
