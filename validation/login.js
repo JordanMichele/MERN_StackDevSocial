889fbafcb0057c51a4b33591356a791db890c961
Jordan Michele <jordanmichele32@gmail.com>
2019-04-20 17:19:22 -0400

    first phase of validation complete

--- validation/login.js
+++ validation/login.js
@@ -6,10 +6,7 @@ module.exports = function validateLoginInput(data) {
 
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
-  // Using Validator to check if the email field has been inputted
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  }
+
   // Using Validator to check if the email field is valid
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
@@ -17,6 +14,10 @@ module.exports = function validateLoginInput(data) {
   // Using Validator to check if the password field has been inputted
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
+  }
+  // Using Validator to check if the email field has been inputted
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
   }
   // Using Validator to check
   return {
