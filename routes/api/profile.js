a13c5a76e6a5d099d9124d340ccd3c0d02b1240a
Jordan Michele <jordanmichele32@gmail.com>
2019-04-25 18:39:53 -0400

    finished first profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -22,11 +22,17 @@ router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Profile.findOne({ user: req.user.id }).then(profile => {
-      if (!profile) {
-        return res.status(404).json();
-      }
-    });
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
   }
 );
 
